db6e8a489d1c24ddabd2d51b897d89ddd3fc955f
DongwooKim <kimphysicsman@gmail.com>
2022-06-24 10:22:09 +0900

    first commit

---
+++ post/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from .models import Company, BusinessArea, JobPost, JobType, SkillSet, JobPostSkillSet
+
+
+class BusinessAreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessArea
+        fields = ['area']
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['company_name']
+
+class JobTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobType
+        fields = ['job_type']
+
+class JobPostSerializer(serializers.ModelSerializer):
+    job_type = JobTypeSerializer()
+    company = CompanySerializer()
+    skillsets = serializers.SerializerMethodField()
+
+    def get_skillsets(self, obj):
+        skillsets = obj.skillset_set.all()
+        name_list = [skillset.name for skillset in skillsets]
+        return name_list
+
+    class Meta:
+        model = JobPost
+        fields = ['job_type', 'company', 'job_description', 'salary', 'skillsets']
+
+    def create(self, validated_data):
+        company = validated_data.pop('company')
+        company = Company.objects.get(company_name=company['company_name'])
+
+        job_type = validated_data.pop('job_type')
+        job_type = JobType.objects.get(job_type=job_type['job_type'])
+
+        job_post = JobPost.objects.create(company=company, job_type=job_type, **validated_data)
+        job_post.save()
+
+        return job_post
+
+
+class SkillSetSerializer(serializers.ModelSerializer):
+    job_posts = JobPostSerializer(many=True)
+    class Meta:
+        model = SkillSet
+        fields = ['name' ,'job_posts']
+
+class JobPostSkillSetSerializer(serializers.ModelSerializer):
+    job_posts = JobPostSerializer()
+    skill_set = SkillSetSerializer()
+    class Meta:
+        model = SkillSet
+        fields = ['skill_set' ,'job_posts']
---
+++ post/views.py
@@ -0,0 +1,58 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from .models import (
+    JobPostSkillSet,
+    JobType,
+    JobPost,
+    Company,
+    SkillSet
+)
+from django.db.models.query_utils import Q
+from .serializers import JobPostSerializer, CompanySerializer, JobTypeSerializer
+
+
+class SkillView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        skills = self.request.query_params.getlist('skills', '')
+       
+
+        query = Q()
+        for skill in skills:
+            skill_set = SkillSet.objects.get(name=skill)
+            query = query | Q(skillset=skill_set)
+        job_posts = JobPost.objects.filter(query)
+
+        return Response(JobPostSerializer(job_posts, many=True).data, status=status.HTTP_200_OK)
+
+
+class JobView(APIView):
+
+    def post(self, request):
+        job_type = int( request.data.get("job_type", None) )
+        company_name = request.data.get("company_name", None)
+
+        try:
+            job_type = JobType.objects.get(id=job_type)
+        except:
+            return Response({'message': '해당 job_type 이 없습니다!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            company = Company.objects.get(company_name=company_name)
+        except:
+            company = Company.objects.create(company_name=company_name)
+            company.save()
+        
+        request.data['company'] = CompanySerializer(company).data
+        request.data['job_type'] = JobTypeSerializer(job_type).data
+
+        job_serializer = JobPostSerializer(data=request.data)
+        if job_serializer.is_valid():
+            job_serializer.save()
+            return Response({'message': '저장 완료!'}, status=status.HTTP_200_OK)
+
+        return Response(job_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
